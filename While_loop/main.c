49233d0e3aa2abf6d6c25ba5c0f9f034dd859b44
Abdullah Al Abid <abdullahabid1660@gmail.com>
2022-09-04 01:43:14 +0600

    Add files via upload

---
+++ While_loop/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  While_loop
+//
+//  Created by Abdullah Al Abid on 4/9/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int x = 20;
+    while(x >= 15){
+        printf("Hi\n");
+        printf("Hello\n");
+        
+        x-=2;
+    }
+    return 0;
+}
